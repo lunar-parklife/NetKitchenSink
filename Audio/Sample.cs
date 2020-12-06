12fc2fe648a76fe6ea9c6f29a03dd25089592364
Radiotroph <radiotroph@icloud.com>
2020-12-06 12:58:55 -0800

    Added the actual body of sample, or at least the start

--- Audio/Sample.cs
+++ Audio/Sample.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Runtime.ConstrainedExecution;
 namespace RKitchen.Audio
 {
+    [ReliabilityContract (Consistency.WillNotCorruptState, Cer.Success)]
     [Serializable]
     public readonly struct Sample
     {
+        public Sample(int pitch, uint length)
+        {
+            Pitch = pitch;
+            Length = length;
+        }
+
         public int Pitch { get; }
+        public uint Length { get; }
+
+        public override string ToString() => $"p{Pitch}-l{Length}";
     }
 }
