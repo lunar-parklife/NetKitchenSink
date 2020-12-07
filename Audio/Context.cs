d17b3c7d388006d9b08347495466fc2d8d484417
Radiotroph <radiotroph@icloud.com>
2020-12-06 19:44:51 -0800

    Work

--- Audio/Context.cs
+++ Audio/Context.cs
@@ -5,5 +5,7 @@ namespace RKitchen.Audio
     {
         string Name { get; }
         IAudioDevice ConnectNew(string desc, params object[] args);
+        IAudioDevice ConnectExist(string desc);
+        void DestroyDevice(string desc);
     }
 }
--- AudioPA/Context.cs
+++ AudioPA/Context.cs
@@ -1,13 +1,32 @@
 using System;
+using System.Collections.Generic;
 namespace RKitchen.Audio.PA
 {
     public sealed class PaContext : IAudioContext
     {
+        private IntPtr contextData;
+        private IList<IAudioDevice> devices;
+
+        public PaContext()
+        {
+            //
+        }
+
         public string Name { get; }
 
         public IAudioDevice ConnectNew(string desc, params object[] args)
         {
             throw new NotImplementedException();
+        }
+
+        public IAudioDevice ConnectExist(string desc)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DestroyDevice(string desc)
+        {
+            //
         }
 
         public void Dispose()
