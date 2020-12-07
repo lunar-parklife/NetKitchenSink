a28195f116a282255905bf56bd6de9415c04e025
Radiotroph <radiotroph@icloud.com>
2020-12-06 19:32:54 -0800

    Worked on PA simple backend

--- AudioPA/Platform/Simple.cs
+++ AudioPA/Platform/Simple.cs
@@ -5,6 +5,22 @@ namespace RKitchen.Audio.PA.Platform
     internal static class Simple
     {
         [DllImport("pulseaudio")]
-        public extern static void pa_sample_free(IntPtr s);
+        internal extern static IntPtr pa_simple_new(
+            [MarshalAs(UnmanagedType.AnsiBStr)] string server,
+            [MarshalAs(UnmanagedType.AnsiBStr)] string name,
+            int dir,
+            [MarshalAs(UnmanagedType.AnsiBStr)] string dev
+        );
+
+        [DllImport("pulseaudio")]
+        internal extern static void pa_sample_free(IntPtr s);
+
+        [DllImport("puseaudio")]
+        internal extern static int pa_simple_write(
+            IntPtr s,
+            IntPtr data,
+            uint bytes,
+            IntPtr error
+        );
     }
 }
